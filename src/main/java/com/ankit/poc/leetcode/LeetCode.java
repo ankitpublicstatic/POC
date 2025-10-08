5a4a9cf0b5596fa592f279c3d6b615f92e76d252
ankitpublicstatic <ankitpublicstatic@gmail.com>
2025-10-08 19:31:04 +0530

    Implemented find longest sub sequence from given array.

--- src/main/java/com/ankit/poc/leetcode/LeetCode.java
+++ src/main/java/com/ankit/poc/leetcode/LeetCode.java
@@ -363,4 +363,20 @@ public class LeetCode {
     }
     return -1;
   }
+
+  public int lengthOfLIS(int[] nums) {
+    int[] lis = new int[nums.length];
+    Arrays.fill(lis, 1);
+    int max = 1;
+
+    for (int i = 1; i < nums.length; i++) {
+      for (int j = 0; j < i; j++) {
+        if (nums[i] > nums[j]) {
+          lis[i] = Math.max(lis[i], 1 + lis[j]);
+          max = Math.max(max, lis[i]);
+        }
+      }
+    }
+    return max;
+  }
 }
