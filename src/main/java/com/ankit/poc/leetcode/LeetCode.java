1e65bcbe9b89957e298d531df68e9af17fb9eb14
ankitpublicstatic <ankitpublicstatic@gmail.com>
2025-10-10 00:22:07 +0530

    Implemented word break solutions.

--- src/main/java/com/ankit/poc/leetcode/LeetCode.java
+++ src/main/java/com/ankit/poc/leetcode/LeetCode.java
@@ -395,4 +395,20 @@ public class LeetCode {
     }
     return matrix[0][0];
   }
+
+  public boolean wordBreak(String s, List<String> wordDict) {
+    boolean[] dp = new boolean[s.length() + 1];
+    dp[0] = true;
+    Set<String> wordSet = new HashSet<>(wordDict);
+
+    for (int i = 1; i <= s.length(); i++) {
+      for (int j = i - 1; j >= 0; j--) {
+        if (dp[j] && wordSet.contains(s.substring(j, i))) {
+          dp[i] = true;
+          break;
+        }
+      }
+    }
+    return dp[s.length()];
+  }
 }
