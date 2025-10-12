a589bbcf26caf40f0599364ed5661ebc98c1edc3
ankitpublicstatic <ankitpublicstatic@gmail.com>
2025-10-12 08:37:50 +0530

    Implemented Pacific Atlantic Water Flow

--- src/main/java/com/ankit/poc/leetcode/LeetCode.java
+++ src/main/java/com/ankit/poc/leetcode/LeetCode.java
@@ -598,4 +598,80 @@ public class LeetCode {
     courseGraph.put(currentCourse, null);
     return true;
   }
+
+  public List<List<Integer>> pacificAtlantic(int[][] heights) {
+    if (heights.length == 0 || heights[0].length == 0) {
+      return new ArrayList<>();
+    }
+    int row = heights.length, col = heights[0].length;
+
+    boolean[][] pacificReachalbe = new boolean[row][col], atlanticReachable = new boolean[row][col];
+
+    for (int i = 0; i < row; i++) {
+      dfs(i, 0, pacificReachalbe, heights);
+      dfs(i, col - 1, atlanticReachable, heights);
+    }
+
+    for (int i = 0; i < col; i++) {
+      dfs(0, i, pacificReachalbe, heights);
+      dfs(row - 1, i, atlanticReachable, heights);
+    }
+
+    List<List<Integer>> result = new ArrayList<>();
+
+    for (int i = 0; i < row; i++) {
+      for (int j = 0; j < col; j++) {
+        if (pacificReachalbe[i][j] && atlanticReachable[i][j]) {
+          result.add(List.of(i, j));
+        }
+      }
+    }
+    return result;
+
+  }
+
+
+  private void dfs(int row, int col, boolean[][] reachalbe, int[][] heights) {
+    int[][] directions = new int[][] {{0, 1}, {1, 0}, {-1, 0}, {0, -1}};
+    reachalbe[row][col] = true;
+
+    for (int[] dir : directions) {
+      int newRow = row + dir[0], newCol = col + dir[1];
+
+      if (newRow < 0 || newRow >= heights.length || newCol < 0 || newCol >= heights[0].length) {
+        continue;
+      }
+
+      if (reachalbe[newRow][newCol]) {
+        continue;
+      }
+
+      if (heights[newRow][newCol] < heights[row][col]) {
+        continue;
+      }
+      dfs(newRow, newCol, reachalbe, heights);
+
+    }
+  }
+
+  private void dfs2(int row, int col, boolean[][] reachalbe, int[][] heights) {
+    int[][] directions = new int[][] {{0, 1}, {1, 0}, {-1, 0}, {0, -1}};
+    reachalbe[row][col] = true;
+
+    for (int[] dir : directions) {
+      int newRow = row + dir[0], newCol = col + dir[1];
+
+      if (newRow < 0 || newRow >= heights.length || newCol < 0 || newCol >= heights[0].length) {
+        continue;
+      }
+
+      if (reachalbe[newRow][newCol]) {
+        continue;
+      }
+
+      if (heights[newRow][newCol] >= heights[row][col]) {
+        dfs(newRow, newCol, reachalbe, heights);
+      }
+    }
+  }
 }
