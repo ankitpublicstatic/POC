93f45e6771ab1e1bb3d79cb21537675624238d1d
ankitpublicstatic <ankitpublicstatic@gmail.com>
2025-10-14 08:34:21 +0530

    Implemented insert interval- realtime problem like calendar and schedule the meetings

--- src/main/java/com/ankit/poc/leetcode/LeetCode.java
+++ src/main/java/com/ankit/poc/leetcode/LeetCode.java
@@ -895,4 +895,38 @@ public class LeetCode {
       }
     }
   }
+
+  // insert interval- real time problem like calendar and schedule the meetings
+
+  public int[][] insert(int[][] intervals, int[] newInterval) {
+    int newStart = newInterval[0], newEnd = newInterval[1], left = 0, right = intervals.length;
+    LinkedList<int[]> output = new LinkedList<>();
+
+    while (left < right && newStart > intervals[left][0]) {
+      output.add(intervals[left]);
+      left++;
+    }
+    int[] interval = new int[2];
+    if (output.isEmpty() || output.getLast()[1] < newStart) {
+      output.add(newInterval);
+    } else {
+      interval = output.removeLast();
+      interval[1] = Math.max(interval[1], newEnd);
+      output.add(interval);
+    }
+
+    while (left < right) {
+      interval = intervals[left];
+      left++;
+      int start = interval[0], end = interval[1];
+      if (output.getLast()[1] < start) {
+        output.add(interval);
+      } else {
+        interval = output.removeLast();
+        interval[1] = Math.max(interval[1], end);
+        output.add(interval);
+      }
+    }
+    return output.toArray(new int[output.size()][2]);
+  }
 }
