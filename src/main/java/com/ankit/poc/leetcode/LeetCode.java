3bb080a1dfd49a40e7ae65c20f6f10a14b971841
ankitpublicstatic <ankitpublicstatic@gmail.com>
2025-10-14 12:35:07 +0530

    Implemented meeting rooms ii

--- src/main/java/com/ankit/poc/leetcode/LeetCode.java
+++ src/main/java/com/ankit/poc/leetcode/LeetCode.java
@@ -975,4 +975,34 @@ public class LeetCode {
     }
     return true;
   }
+
+  // meeting rooms ii
+
+  public int minMeetingRooms(int[][] intervals) {
+    if (intervals.length == 0) {
+      return 0;
+    }
+    int[] start = new int[intervals.length], end = new int[intervals.length];
+
+    for (int i = 0; i < intervals.length; i++) {
+      start[i] = intervals[i][0];
+      end[i] = intervals[i][1];
+    }
+
+    Arrays.sort(start);
+    Arrays.sort(end);
+
+    int meetingRoomsCount = 0, startptr = 0, endptr = 0;
+
+    while (startptr < start.length) {
+      if (start[startptr] >= end[endptr]) {
+        meetingRoomsCount--;
+        endptr++;
+      }
+      meetingRoomsCount++;
+      startptr++;
+    }
+
+    return meetingRoomsCount;
+  }
 }
