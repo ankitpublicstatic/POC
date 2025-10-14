a1c40ee746ea790a4108a2a297ed22ea0a440921
ankitpublicstatic <ankitpublicstatic@gmail.com>
2025-10-14 12:01:14 +0530

    Implemented metting rooms.

--- src/main/java/com/ankit/poc/leetcode/LeetCode.java
+++ src/main/java/com/ankit/poc/leetcode/LeetCode.java
@@ -960,10 +960,19 @@ public class LeetCode {
       } else {
         prev = i;
       }
-
     }
-
-
     return count;
+  }
+
+  // meeting-rooms
+  public boolean canAttendMeetings(int[][] intervals) {
+    Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
+
+    for (int i = 0; i < intervals.length; i++) {
+      if (intervals[i][1] > intervals[i + 1][0]) {
+        return false;
+      }
+    }
+    return true;
   }
 }
