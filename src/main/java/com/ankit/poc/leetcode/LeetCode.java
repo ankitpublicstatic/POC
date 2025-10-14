52cbdc0e9d72e244e601cacc5a24d64eed6fff90
ankitpublicstatic <ankitpublicstatic@gmail.com>
2025-10-14 12:46:25 +0530

    Implemented Reverse Linked List Node

--- src/main/java/com/ankit/poc/leetcode/LeetCode.java
+++ src/main/java/com/ankit/poc/leetcode/LeetCode.java
@@ -1005,4 +1005,34 @@ public class LeetCode {
 
     return meetingRoomsCount;
   }
+
+  public class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {}
+
+    ListNode(int val) {
+      this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+      this.val = val;
+      this.next = next;
+    }
+  }
+
+  public ListNode reverseList(ListNode head) {
+
+    ListNode prev = null, curr = head;
+    while (curr != null) {
+      ListNode temp = curr.next;
+      curr.next = prev;
+      prev = curr;
+      curr = temp;
+    }
+    return prev;
+
+  }
+
 }
