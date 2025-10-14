8346eec6e77f6ca87c6dee5bc728fef8d4e87fa7
ankitpublicstatic <ankitpublicstatic@gmail.com>
2025-10-14 15:03:28 +0530

    Implemented linked-list-cycle

--- src/main/java/com/ankit/poc/leetcode/LeetCode.java
+++ src/main/java/com/ankit/poc/leetcode/LeetCode.java
@@ -1035,4 +1035,24 @@ public class LeetCode {
 
   }
 
+  public boolean hasCycle(ListNode head) {
+    if (head == null) {
+      return false;
+    }
+    ListNode slow = head, fast = head.next;
+
+    while (slow != null || fast != null) {
+      if (fast == null || fast.next == null) {
+        return false;
+      }
+      if (fast == slow) {
+        return true;
+      }
+
+      slow = slow.next;
+      fast = fast.next.next;
+    }
+    return false;
+  }
+
 }
