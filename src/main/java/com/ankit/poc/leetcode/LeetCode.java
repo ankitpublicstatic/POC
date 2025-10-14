58f0b4a52b21ae3105cb8a773391923805d70124
ankitpublicstatic <ankitpublicstatic@gmail.com>
2025-10-14 11:56:27 +0530

    Implmented non-overlapping-intervals

--- src/main/java/com/ankit/poc/leetcode/LeetCode.java
+++ src/main/java/com/ankit/poc/leetcode/LeetCode.java
@@ -943,4 +943,27 @@ public class LeetCode {
     return result.toArray(new int[result.size()][]);
 
   }
+
+  public int eraseOverlapIntervals(int[][] intervals) {
+    if (intervals.length == 0) {
+      return 0;
+    }
+    Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
+    int prev = 0, count = 0;
+
+    for (int i = 1; i < intervals.length; i++) {
+      if (intervals[prev][1] > intervals[i][0]) {
+        if (intervals[prev][1] > intervals[i][1]) {
+          prev = i;
+        }
+        count++;
+      } else {
+        prev = i;
+      }
+
+    }
+
+
+    return count;
+  }
 }
