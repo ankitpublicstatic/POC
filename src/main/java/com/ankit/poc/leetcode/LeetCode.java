d7bf476859198ee7c69595e6d602e37bfff949d8
ankitpublicstatic <ankitpublicstatic@gmail.com>
2025-10-15 06:24:36 +0530

    Implemented remove-nth-node-from-end-of-list

--- src/main/java/com/ankit/poc/leetcode/LeetCode.java
+++ src/main/java/com/ankit/poc/leetcode/LeetCode.java
@@ -1092,4 +1092,23 @@ public class LeetCode {
     }
     return dummy.next;
   }
+
+  // 19 remove-nth-node-from-end-of-list
+  public ListNode removeNthFromEnd(ListNode head, int n) {
+    ListNode dummy = new ListNode(1);
+
+    dummy.next = head;
+    ListNode front = dummy, back = dummy;
+
+    for (int i = 0; i <= n; i++) {
+      front = front.next;
+    }
+
+    while (front != null) {
+      front = front.next;
+      back = back.next;
+    }
+    back.next = back.next.next;
+    return dummy.next;
+  }
 }
