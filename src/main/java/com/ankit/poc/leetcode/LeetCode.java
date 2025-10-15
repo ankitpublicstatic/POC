9ddd1a679becf307d1acf3c133529d49e5b6794c
ankitpublicstatic <ankitpublicstatic@gmail.com>
2025-10-15 07:34:11 +0530

    Implemented set-matrix-zeroes

--- src/main/java/com/ankit/poc/leetcode/LeetCode.java
+++ src/main/java/com/ankit/poc/leetcode/LeetCode.java
@@ -1144,4 +1144,45 @@ public class LeetCode {
     }
 
   }
+  // 73 set-matrix-zeroes
+
+  public void setZeroes(int[][] matrix) {
+    boolean firstColumn = false;
+    int row = matrix.length;
+    int col = matrix[0].length;
+
+    for (int i = 0; i < row; i++) {
+      if (matrix[i][0] == 0) {
+        firstColumn = true;
+      }
+
+      for (int j = 0; j < col; j++) {
+        if (matrix[i][j] == 0) {
+          matrix[i][0] = 0;
+          matrix[0][j] = 0;
+        }
+      }
+    }
+
+    for (int i = 1; i < row; i++) {
+
+      for (int j = 1; j < col; j++) {
+        if (matrix[i][0] == 0 || matrix[0][j] == 0) {
+          matrix[i][j] = 0;
+        }
+      }
+    }
+
+    if (matrix[0][0] == 0) {
+      for (int j = 0; j < col; j++) {
+        matrix[0][j] = 0;
+      }
+    }
+
+    if (firstColumn) {
+      for (int i = 0; i < row; i++) {
+        matrix[i][0] = 0;
+      }
+    }
+  }
 }
