0a0db1efa394596be42e1293b822b38809af7a3e
ankitpublicstatic <ankitpublicstatic@gmail.com>
2025-10-16 08:20:40 +0530

    Implemented longest-repeating-character-replacement

--- src/main/java/com/ankit/poc/leetcode/LeetCode.java
+++ src/main/java/com/ankit/poc/leetcode/LeetCode.java
@@ -1308,4 +1308,22 @@ public class LeetCode {
     return ans;
 
   }
+
+  // 424 longest-repeating-character-replacement/description
+
+  public int characterReplacement(String s, int k) {
+    int[] occurance = new int[26];
+    int left = 0, right = 0, ans = 0, maxOccurance = 0;
+
+    for (right = 0; right < s.length(); right++) {
+      maxOccurance = Math.max(maxOccurance, ++occurance[s.charAt(right) - 'A']);
+
+      if (right - left + 1 - maxOccurance > k) {
+        occurance[s.charAt(left) - 'A']--;
+        left++;
+      }
+      ans = Math.max(ans, right - left + 1);
+    }
+    return ans;
+  }
 }
