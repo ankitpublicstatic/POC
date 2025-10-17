de844fe55b515c0c591bb21912df859770ccb826
ankitpublicstatic <ankitpublicstatic@gmail.com>
2025-10-17 10:27:02 +0530

    Implemented longest-palindromic-substring

--- src/main/java/com/ankit/poc/leetcode/LeetCode.java
+++ src/main/java/com/ankit/poc/leetcode/LeetCode.java
@@ -1519,4 +1519,34 @@ public class LeetCode {
     }
     return true;
   }
+
+  // 5 longest-palindromic-substring
+
+  public String longestPalindrome(String s) {
+    if (s == null || s.length() < 1) {
+      return "";
+    }
+    int left = 0, right = 0;
+    for (int i = 0; i < s.length(); i++) {
+      int len1 = checkPalindrome(s, i, i), len2 = checkPalindrome(s, i, i + 1),
+          len = Math.max(len1, len2);
+
+      if (len > right - left) {
+        left = i - (len - 1) / 2;
+        right = i + len / 2;
+      }
+    }
+
+    return s.substring(left, right + 1);
+  }
+
+  private int checkPalindrome(String s, int left, int right) {
+    int L = left, R = right;
+
+    while (L >= 0 && R < s.length() && s.charAt(L) == s.charAt(R)) {
+      L--;
+      R++;
+    }
+    return R - L - 1;
+  }
 }
