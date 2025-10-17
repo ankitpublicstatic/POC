4f652f79420f3b24166695c35ff874d1a0870f3a
ankitpublicstatic <ankitpublicstatic@gmail.com>
2025-10-17 08:07:40 +0530

    Implemented group anagrams

--- src/main/java/com/ankit/poc/leetcode/LeetCode.java
+++ src/main/java/com/ankit/poc/leetcode/LeetCode.java
@@ -1435,5 +1435,39 @@ public class LeetCode {
     return true;
   }
 
+  // 49 group-anagrams
+
+  public List<List<String>> groupAnagrams(String[] strs) {
+    if (strs.length == 0) {
+      return new ArrayList<>();
+    }
+    Map<String, List> ansMap = new HashMap<>();
+    int[] count = new int[26];
+
+    for (String s : strs) {
+      Arrays.fill(count, 0);
+
+      for (char c : s.toCharArray()) {
+        count[c - 'a']++;
+      }
+
+      StringBuilder sb = new StringBuilder("");
+
+      for (int i = 0; i < 26; i++) {
+        sb.append("#");
+        sb.append(count[i]);
+      }
+
+      String key = sb.toString();
+
+      if (!ansMap.containsKey(key)) {
+        ansMap.put(key, new ArrayList<>());
+      }
+      if (ansMap.get(key) != null) {
+        ansMap.get(key).add(s);
+      }
+    }
+    return new ArrayList(ansMap.values());
+  }
 
 }
