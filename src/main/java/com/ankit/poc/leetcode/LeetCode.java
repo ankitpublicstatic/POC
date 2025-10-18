9373810a33f4761d473ade56738e4dc36a1a91e4
ankitpublicstatic <ankitpublicstatic@gmail.com>
2025-10-18 16:08:04 +0530

    Implemented maximum-depth-of-binary-tree

--- src/main/java/com/ankit/poc/leetcode/LeetCode.java
+++ src/main/java/com/ankit/poc/leetcode/LeetCode.java
@@ -1602,5 +1602,35 @@ public class LeetCode {
     return Arrays.asList(split);
   }
 
+  // Definition for a binary tree node.
+  public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {}
+
+    TreeNode(int val) {
+      this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+      this.val = val;
+      this.left = left;
+      this.right = right;
+    }
+  }
+
+  // 104 maximum-depth-of-binary-tree
+  public int maxDepth(TreeNode root) {
+    if (root == null) {
+      return 0;
+    } else {
+      int leftmax = maxDepth(root.left);
+      int rightmax = maxDepth(root.right);
+
+      return Math.max(leftmax, rightmax) + 1;
+    }
+  }
 
 }
