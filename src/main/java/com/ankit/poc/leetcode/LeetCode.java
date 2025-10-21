a77a3baa2be00a64821394aae8cc114236124f9f
ankitpublicstatic <ankitpublicstatic@gmail.com>
2025-10-21 17:29:02 +0530

    implemented number-of-1-bits

--- src/main/java/com/ankit/poc/leetcode/LeetCode.java
+++ src/main/java/com/ankit/poc/leetcode/LeetCode.java
@@ -2016,4 +2016,15 @@ public class LeetCode {
 
   }
 
+  // 191 number-of-1-bits
+  public int hammingWeight(int n) {
+    int count = 0;
+    while (n != 0) {
+      count++;
+      n = n & n - 1;
+    }
+    return count;
+
+  }
+
 }
