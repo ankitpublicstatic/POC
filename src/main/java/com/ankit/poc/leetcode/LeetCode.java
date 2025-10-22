6956ba61bc7bc453d61ca100ebf5792132e13baa
ankitpublicstatic <ankitpublicstatic@gmail.com>
2025-10-22 08:55:40 +0530

    Implemented reverse-bits

--- src/main/java/com/ankit/poc/leetcode/LeetCode.java
+++ src/main/java/com/ankit/poc/leetcode/LeetCode.java
@@ -2053,4 +2053,17 @@ public class LeetCode {
     return xOr;
 
   }
+
+  // 190 reverse-bits/
+  public int reverseBits(int n) {
+    int reverse = 0;
+    for (int i = 0; i < 32; i++) {
+      reverse = reverse << 1;
+      reverse = reverse | n & 1;
+      n = n >> 1;
+    }
+    return reverse;
+
+  }
+
 }
