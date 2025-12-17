287aed1dddd7ca04263049c40f930d9a6c5b3f66
ankitpublicstatic <ankitpublicstatic@gmail.com>
2025-12-17 12:00:55 +0530

    Updated

---
+++ src/main/java/OverlapingTest.java
@@ -0,0 +1,87 @@
+import java.util.Arrays;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map.Entry;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+
+public class OverlapingTest {
+  public static void main(String[] args) {
+    List<String> list =
+        Arrays.asList("Chennai", "Bangalore", "Chennai", "Hyderabad", "Bangalore", "Hyderabad");
+    // find second duplicate element from the list
+    String str = "TESTDEMOTESTDEMO";
+    String sub = "DEMO";
+
+    System.out.println(countOverlaping("AAAA", "AA", false));
+  }
+
+  public static Entry<String, Long> getDuplicateByIndex(List<String> list, int index) {
+    return list.stream()
+        .collect(Collectors.groupingBy(x -> x, LinkedHashMap::new, Collectors.counting()))
+        .entrySet().stream().filter(x -> x.getValue() > 1).collect(Collectors.toList())
+        .get(index - 1);
+  }
+  // 🎯 Explanation (How to think about the problem?)Step-by-step thought process:
+
+  // You need to check how many times "DEMO" appears inside a bigger string.
+
+  // A substring can be searched using indexOf. Every time you find one occurrence, increase the
+  // count.
+
+  // Move the index forward to continue searching.
+
+  // Continue until no more substring is found (indexOf returns -1).
+
+  public static int countOverlapingByReplace(String str, String sub) {
+    return (str.length() - str.replace(sub, "").length()) / sub.length();
+  }
+
+  public static int countOverlaping(String str, String sub, boolean allowOverlap) {
+
+    if (str == null || sub == null || sub.isEmpty()) {
+      return 0;
+    }
+    int count = 0, index = 0;
+
+    while ((index = str.indexOf(sub, index)) != -1) {
+      count++;
+      index = index + (allowOverlap ? 1 : sub.length());
+    }
+    return count;
+  }
+
+  public static int countOverlapingByRegex(String str, String sub) {
+    int count = 0;
+    Matcher matcher = Pattern.compile(sub).matcher(str);
+    while (matcher.find()) {
+      count++;
+    }
+    return count;
+  }
+}
+
+
+
+// EMP
+//
+// NAME ID MANAGERID
+//
+// RAKESH 1
+//
+// ABHISHEK 2 1
+//
+// PRAKASH 3 2
+//
+// MANJU 4 3
+//
+// SHINATH 5 2
+//
+// Get employees and their manager names ?
+//
+// select e.name as employee_name, e.name as manager_name from emp e
+// left join emp r on e.managerid = r.id;
+//
+
+
