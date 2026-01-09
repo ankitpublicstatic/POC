0c288b4e3e7b50e598b8ccc3784145e95c3bf8e8
ankitpublicstatic <ankitpublicstatic@gmail.com>
2026-01-09 10:57:45 +0530

    Updated

---
+++ src/main/java/com/ankit/poc/thread/VirtualThread.java
@@ -0,0 +1,44 @@
+package com.ankit.poc.thread;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class VirtualThread {
+  public static void main(String[] args) throws InterruptedException {
+    Thread vThread = Thread.ofVirtual().name("custom-virtual-thread").start(() -> {
+      System.out.println("Running in: " + Thread.currentThread().getName());
+      try {
+        Thread.sleep(1000);
+      } catch (InterruptedException e) {
+        Thread.currentThread().interrupt();
+      }
+    });
+    vThread.join();// wait for completion
+  }
+
+  // Millions of virtual threads vs platform threads
+
+  public void manyVirtualThreads() throws InterruptedException {
+    int taskCount = 100_000_00;// can go higher in practice
+    List<Thread> threads = new ArrayList<>(taskCount);
+    for (int i = 0; i < taskCount; i++) {
+      int id = i;
+      Thread thread = Thread.ofVirtual().start(() -> {
+        // Simulate blocking I/O
+        try {
+          Thread.sleep(1000);
+          // In real code: call DB, HTTP Service, etc.
+          // httpClient.send(request, BodyHandlers.ofString());
+        } catch (InterruptedException e) {
+          Thread.currentThread().interrupt();
+        }
+        System.out.println("Task " + id + " done on " + Thread.currentThread().getName());
+      });
+      threads.add(thread);
+    }
+    for (Thread thread : threads) {
+      thread.join();
+    }
+    System.out.println("All virtual thread tasks completed.");
+  }
+}
